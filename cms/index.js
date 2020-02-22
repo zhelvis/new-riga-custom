2d8a1a694e69d3b2d8ff908a28d3e1f48ab7d2ff
Vladimir Zhelvis <war1lok@yandex.ru>
2020-02-22 19:02:38 +0300

    config CMS

--- cms/index.js
+++ cms/index.js
@@ -1,23 +1,29 @@
-const { Keystone } = require('@keystonejs/keystone');
-const { PasswordAuthStrategy } = require('@keystonejs/auth-password');
-const { Text, Checkbox, Password } = require('@keystonejs/fields');
-const { GraphQLApp } = require('@keystonejs/app-graphql');
-const { AdminUIApp } = require('@keystonejs/app-admin-ui');
-const initialiseData = require('./initial-data');
+const { Keystone } = require("@keystonejs/keystone");
+const { PasswordAuthStrategy } = require("@keystonejs/auth-password");
+const {
+  Text,
+  Checkbox,
+  Password,
+  Relationship
+} = require("@keystonejs/fields");
+const { Wysiwyg } = require('@keystonejs/fields-wysiwyg-tinymce');
+const { GraphQLApp } = require("@keystonejs/app-graphql");
+const { AdminUIApp } = require("@keystonejs/app-admin-ui");
+const initialiseData = require("./initial-data");
 
-const { MongooseAdapter: Adapter } = require('@keystonejs/adapter-mongoose');
+const { MongooseAdapter: Adapter } = require("@keystonejs/adapter-mongoose");
 
 const PROJECT_NAME = "New Riga Custom";
-
 
 const keystone = new Keystone({
   name: PROJECT_NAME,
   adapter: new Adapter(),
-  onConnect: initialiseData,
+  onConnect: initialiseData
 });
 
 // Access control functions
-const userIsAdmin = ({ authentication: { item: user } }) => Boolean(user && user.isAdmin);
+const userIsAdmin = ({ authentication: { item: user } }) =>
+  Boolean(user && user.isAdmin);
 const userOwnsItem = ({ authentication: { item: user } }) => {
   if (!user) {
     return false;
@@ -31,30 +37,85 @@ const userIsAdminOrOwner = auth => {
 };
 const access = { userIsAdmin, userOwnsItem, userIsAdminOrOwner };
 
-keystone.createList('User', {
+keystone.createList("User", {
+  label: "Пользователи",
+  singular: "Пользователь",
+  plural: "Пользователи",
   fields: {
-    name: { type: Text },
+    name: { type: Text, label: "Имя" },
     email: {
       type: Text,
       isUnique: true,
+      label: "Почта"
     },
-    isAdmin: { type: Checkbox },
+    isAdmin: { type: Checkbox, label: "Админ?" },
     password: {
       type: Password,
-    },
+      label: "Пароль"
+    }
   },
   access: {
     read: access.userIsAdminOrOwner,
     update: access.userIsAdminOrOwner,
     create: access.userIsAdmin,
     delete: access.userIsAdmin,
-    auth: true,
-  },
+    auth: true
+  }
+});
+
+keystone.createList("ServiceGroup", {
+  label: "Разделы услуг",
+  singular: "Раздел услуг",
+  plural: "Разделы услуг",
+  fields: {
+    name: { type: Text, label: "Раздел" },
+    services: {
+      type: Relationship,
+      ref: "Service.group",
+      many: true,
+      label: "Услуги"
+    }
+  }
+});
+
+keystone.createList("Service", {
+  label: "Услуги",
+  singular: "Услуга",
+  plural: "Услуги",
+  fields: {
+    name: { type: Text, label: "Наименование" },
+    group: { type: Relationship, ref: "ServiceGroup.services", label: "Раздел" }
+  }
+});
+
+keystone.createList("ContentBlock", {
+  label: "Контент",
+  singular: "Контент",
+  plural: "Контент",
+  fields: {
+    name: { type: Text, label: "Блок" },
+    type: { type: Text, label: "Тип"},
+    body: {
+      type: Wysiwyg
+    },
+  }
+})
+
+keystone.createList("Contact", {
+  label: "Контакты",
+  singular: "Контакт",
+  plural: "Контакты",
+  fields: {
+    name: { type: Text, label: "Имя"},
+    type: { type: Text, label: "Тип"},
+    displayText: { type: Text, label: "Текст" },
+    link: { type: Text, label: "Ссылка" }
+  }
 });
 
 const authStrategy = keystone.createAuthStrategy({
   type: PasswordAuthStrategy,
-  list: 'User',
+  list: "User"
 });
 
 module.exports = {
@@ -63,7 +124,7 @@ module.exports = {
     new GraphQLApp(),
     new AdminUIApp({
       enableDefaultRoute: true,
-      authStrategy,
-    }),
-  ],
+      authStrategy
+    })
+  ]
 };
